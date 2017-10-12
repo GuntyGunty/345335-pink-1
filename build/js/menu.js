64ed6bb43ef890aa2d15da3ede3763d34fbb6616
Gunter <gunter0705@yandex.ru>
2017-10-13 01:59:42 +0300

    Последние доработки

--- build/js/menu.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";var e=document.querySelector(".page-header"),a=document.querySelector(".page-header__toggle");document.querySelector(".packets__link").classList.add("packets__link--nojs"),a.addEventListener("click",function(){e.classList.contains("page-header--closed")?(e.classList.remove("page-header--closed"),e.classList.add("page-header--opened")):(e.classList.add("page-header--closed"),e.classList.remove("page-header--opened"))})}();
-//# sourceMappingURL=menu.js.map
